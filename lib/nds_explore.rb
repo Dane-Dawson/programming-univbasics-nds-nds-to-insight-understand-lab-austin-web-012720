3c1a4a5b6805a66d92234a3bc677f37048db6eae
Dane P Dawson <dane.p.dawson@gmail.com>
2020-01-15 18:35:13 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -9,7 +9,7 @@ end
 def print_first_directors_movie_titles
 row_index = 0 
 while row_index < directors_database[0][:movies][:title].length do
-  puts "\t#{directors_database[0][:movies][:title][row_index]}"
+  puts "#{directors_database[0][:movies][:title][row_index]}"
   row_index += 1
 end
 end
