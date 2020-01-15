e6683648dbeadff028654212b0a1436a6605ef1a
Dane P Dawson <dane.p.dawson@gmail.com>
2020-01-15 18:08:34 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -8,7 +8,7 @@ end
 
 def print_first_directors_movie_titles
 row_index = 0 
-while row < directors_database.length do
+while row_index < directors_database.length do
   puts directors_database[row_index]
   row_index += 1
 end
